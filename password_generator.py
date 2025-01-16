d3b28696c4d6aa6f082d6501dbdafd2328c7fc15
Vadym Makohon <138728243+VadymMakohon@users.noreply.github.com>
2025-01-16 20:31:19 +0100

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -2,7 +2,6 @@ import re
 import secrets
 import string
 
-
 def generate_password(length=16, nums=1, special_chars=1, uppercase=1, lowercase=1):
 
     # Define the possible characters for the password
